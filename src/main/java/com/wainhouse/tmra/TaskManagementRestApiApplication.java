a09c1613c34647c891f1ccbe16c37a1e2ba5b9c6
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-11-08 14:13:02 +0000

    LW: First Commit, moved files to repo

---
+++ src/main/java/com/wainhouse/tmra/TaskManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.wainhouse.tmra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementRestApiApplication.class, args);
+	}
+
+}
